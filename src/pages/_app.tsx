b6b4caf2d1b2f7506e276ef30531d46cdc3ea5e1
softgenai <admin@softgen.ai>
2026-04-19 16:03:55 +0000

    Add google_analytics integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
 import { Toaster } from "@/components/ui/toaster";
+import Script from 'next/script'
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      {/* Google Analytics 4 */}
+<Script
+    id="google-analytics-4"
+    src="https://www.googletagmanager.com/gtag/js?id=G-EFQC79NMSN"
+    strategy="afterInteractive"
+/>
+              <Script
+                  id="google-analytics-4-inline"
+                  strategy="afterInteractive"
+                  dangerouslySetInnerHTML={{
+                      __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-EFQC79NMSN');`
+                  }}
+              />
+{/* End Google Analytics 4 */}
+<Component {...pageProps} />
       <Toaster />
     </>
   );
